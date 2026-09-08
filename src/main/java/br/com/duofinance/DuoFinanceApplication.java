18b050a8aa2978b8d7b10721d99d518715c5f550
Allan Rodrigues <allandrigues@dev.com>
2026-09-08 15:14:11 -0300

    Creating Struct Project

---
+++ src/main/java/br/com/duofinance/DuoFinanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.duofinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoFinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuoFinanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/duofinance/DuoFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.duofinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuoFinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
